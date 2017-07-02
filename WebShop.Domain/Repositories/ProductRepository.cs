1788429694555aaaeffdbba9b4947401b99a1d06
Andy Sweetman <adsweetman@gmail.com>
2017-07-02 08:08:56 +0100

    Renamed data project to Domain

---
+++ WebShop.Domain/Repositories/ProductRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using WebShop.Data.Entities;
+
+namespace WebShop.Data.Repositories
+{
+    public class ProductRepository //: IProductRepository
+    {
+        //public void DeleteProduct(int studentID)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public void Dispose()
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public Product GetProductByID(int studentId)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public IEnumerable<Product> GetProducts()
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public void InsertProduct(Product student)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public void Save()
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public void UpdateProduct(Product student)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}

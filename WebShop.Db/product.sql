2e6a4ebe6f28052914314de2c023ce400664d030
Andy Sweetman <adsweetman@gmail.com>
2017-07-02 09:49:18 +0100

    Added nhibernate support and sql db project

---
+++ WebShop.Db/product.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[product]
+(
+	[id] BIGINT NOT NULL PRIMARY KEY IDENTITY, 
+    [name] NVARCHAR(16) NOT NULL, 
+    [price] REAL NOT NULL
+)
--- WebShop.Domain/Entities/Product.cs
+++ WebShop.Domain/Entities/Product.cs
@@ -6,5 +6,30 @@ namespace WebShop.Data.Entities
 {
     class Product
     {
+        private string id;
+        private string name;
+        private decimal price;
+
+        public Product()
+        {
+        }
+
+        public virtual string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public virtual string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public virtual decimal Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
     }
 }
